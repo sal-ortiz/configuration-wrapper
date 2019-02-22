8a1daf4703aaa93da0668fbcd270ff16b7b55477
slackjockey <this.is.salvador.ortiz@gmail.com>
2019-02-22 12:21:25 -0600

    added some tests

---
+++ spec/configuration_spec.js
@@ -0,0 +1,88 @@
+
+const Path = require('path');
+
+const specPath = Path.join(__dirname);
+const rootPath = Path.join(specPath, '..');
+const supportPath = Path.join(specPath, 'support');
+
+const Configuration = require(Path.join(rootPath, 'configuration.js'));
+
+
+const xmlStub = Path.join(supportPath, 'input.xml');
+const yamlStub = Path.join(supportPath, 'input.yml');
+const jsonStub = Path.join(supportPath, 'input.json');
+
+
+describe(Configuration.name, () => {
+
+  describe('the fromFile(...) static function', () => {
+    let stub;
+    let result;
+
+    beforeEach(() => {
+      result = Configuration.fromFile(stub);
+    });
+
+    describe('given JSON input', () => {
+      stub = jsonStub;
+
+      it('parses the given input', () => {
+        let ary = result.anArray;
+        let obj = result.anObject;
+
+        expect(ary[0]).toEqual(1);
+        expect(ary[1]).toEqual(2);
+        expect(ary[2]).toEqual(3);
+        expect(ary[3]).toEqual(4);
+
+        expect(obj.one).toEqual(1);
+        expect(obj.two).toEqual(2);
+        expect(obj.three).toEqual(3);
+        expect(obj.four).toEqual(4);
+      });
+
+    });
+
+    describe('given XML input', () => {
+      stub = xmlStub;
+
+      it('parses the given input', () => {
+        let ary = result.anArray;
+        let obj = result.anObject;
+
+        expect(ary[0]).toEqual(1);
+        expect(ary[1]).toEqual(2);
+        expect(ary[2]).toEqual(3);
+        expect(ary[3]).toEqual(4);
+
+        expect(obj.one).toEqual(1);
+        expect(obj.two).toEqual(2);
+        expect(obj.three).toEqual(3);
+        expect(obj.four).toEqual(4);
+      });
+
+    });
+
+    describe('given YAML input', () => {
+      stub = yamlStub;
+
+      it('parses the given input', () => {
+        let ary = result.anArray;
+        let obj = result.anObject;
+
+        expect(ary[0]).toEqual(1);
+        expect(ary[1]).toEqual(2);
+        expect(ary[2]).toEqual(3);
+        expect(ary[3]).toEqual(4);
+
+        expect(obj.one).toEqual(1);
+        expect(obj.two).toEqual(2);
+        expect(obj.three).toEqual(3);
+        expect(obj.four).toEqual(4);
+      });
+
+    });
+
+  });
+
+});
